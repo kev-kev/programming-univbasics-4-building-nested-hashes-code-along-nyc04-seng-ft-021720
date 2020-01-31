e63fad5ef576fcc32b97a7e012c5f46174dbb592
Kevin Clark <kevin.clark93@gmail.com>
2020-01-31 20:34:32 +0000

    Automatically backed up by Learn

--- lib/first_challenge.rb
+++ lib/first_challenge.rb
@@ -15,9 +15,11 @@ def first_challenge
         status: "alive"
       }
       hero_friends: [
-        {name: "Benvolio"
-         age: "17"
-         attitude: "worried"
+        {name: "Benvolio",
+        age: "17",
+        attitude: "worried"},
+        {name: "Mercutio"
+        age: "18",
         ]
      }
      capulet: {
