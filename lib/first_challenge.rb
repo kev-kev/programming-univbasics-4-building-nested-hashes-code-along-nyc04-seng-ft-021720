00a344f6a446fc8398792fd54b3172e8d0114b3f
Kevin Clark <kevin.clark93@gmail.com>
2020-01-31 21:16:32 +0000

    Automatically backed up by Learn

--- lib/first_challenge.rb
+++ lib/first_challenge.rb
@@ -1,6 +1,6 @@
 def first_challenge
   epic_tragedy = {
-   :montague => {
+    :montague => {
       :patriarch => {name: "Lord Montague", age: "53"},
       :matriarch => {name: "Lady Montague", age: "54"},
       :hero => {name: "Romeo", age: "15", status: "alive"},
@@ -18,5 +18,5 @@ def first_challenge
           {name: "Nurse", age: "44", attitude: "worried"}
       ]
    }
-}
+  }
 end
