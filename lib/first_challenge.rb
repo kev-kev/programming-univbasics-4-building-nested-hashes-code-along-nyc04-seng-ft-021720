015ce051d9869385800cdb8af85e2c22a82e3477
Kevin Clark <kevin.clark93@gmail.com>
2020-01-31 20:30:50 +0000

    Automatically backed up by Learn

--- lib/first_challenge.rb
+++ lib/first_challenge.rb
@@ -1,3 +1,33 @@
 def first_challenge
-   epic_tragedy = {}
+   epic_tragedy = {
+     montague: {
+      patriarch: {
+        name: "Lord Montague"
+        age: "53"
+      }
+      matriarch: {
+        name: "Lady Montague"
+      }
+      hero: {
+        
+      }
+      hero_friends: [
+        
+        ]
+     }
+     capulet: {
+      patriarch: {
+        
+      }
+      matriarch: {
+        
+      }
+      heroine: {
+        
+      }
+      heroine_friends: [
+        
+        ]
+     }
+   }
 end
